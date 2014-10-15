2a3813e1ee2e32ed01546c7e4f46d16902b53c66
javier gomez <javier.gomez.corio@gmail.com>
2014-10-15 15:04:08 +0200

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+class Calculator
+{
+	public function sum($a, $b)
+	{
+		$result = $a + $b;
+
+		return $result;		
+	}
+
+	public function substract($a, $b)
+	{
+		$result = $a - $b;
+
+		return $result;
+	}
+}
+
+?>
